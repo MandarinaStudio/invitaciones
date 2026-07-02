42038b5707e552c045a5e0c0b7c7765c962b61b8
MandarinaStudio <2854779+MandarinaStudio@users.noreply.github.com>
2026-07-01 21:33:34 -0600

    feat: Hero screen and welcome transition

--- js/main.js
+++ js/main.js
@@ -1,16 +1,80 @@
-async function init(){
+const slug = "demo";
 
-    const response = await fetch("config/invitation.json");
+async function loadInvitation() {
 
-    const invitation = await response.json();
+    try {
 
-    document.getElementById("eventTitle").textContent = invitation.title;
+        const response = await fetch(
+            `invitations/${slug}/config/invitation.json`
+        );
 
-    document.getElementById("eventSubtitle").textContent = invitation.subtitle;
+        if (!response.ok) {
+            throw new Error("No se encontró la invitación.");
+        }
+
+        return await response.json();
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
+}
+
+async function init() {
+
+    document.body.classList.add("lock");
+
+    const invitation = await loadInvitation();
+
+    if (!invitation) return;
+
+    // Welcome
+    document.getElementById("eventTitle").textContent =
+        invitation.title;
+
+    document.getElementById("eventSubtitle").textContent =
+        invitation.subtitle;
 
     document.getElementById("welcome").style.backgroundImage =
-        `url(${invitation.hero})`;
+        `url(invitations/${slug}/img/${invitation.hero})`;
+
+    // Hero
+    document.getElementById("heroTitle").textContent =
+        invitation.title;
+
+    document.getElementById("heroDate").textContent =
+        invitation.subtitle;
+
+    document.getElementById("hero").style.backgroundImage =
+        `url(invitations/${slug}/img/${invitation.hero})`;
 
 }
 
 init();
+
+document
+    .getElementById("enterButton")
+    .addEventListener("click", enterInvitation);
+
+function enterInvitation() {
+
+    const welcome = document.getElementById("welcome");
+    const hero = document.getElementById("hero");
+
+    welcome.classList.add("fade-out");
+
+    setTimeout(() => {
+
+        welcome.style.display = "none";
+
+        document.body.classList.remove("lock");
+
+        hero.classList.add("active");
+
+       
+
+    }, 800);
+
+}
